3c6325e29a252fb8b77654c2ed816f7734c7e800
raquellyc <raquels.castillo.78@gmail.com>
2022-06-22 21:48:07 -0600

    finish css

---
+++ script.js
@@ -0,0 +1,19 @@
+const header = document.getElementById('header')
+const title = document.getElementById('title')
+const excerpt = document.getElementById('excerpt')
+const profile_img = document.getElementById('profile_img')
+const name = document.getElementById('name')
+const date = document.getElementById('date')
+
+const animated_bgs = document.querySelectorAll('.animated-bg')
+const animated_bg_texts = document.querySelectorAll('.animated-bg-text')
+
+function getData() {
+    header.innerHTML = '<img src="https://images.pexels.com/photos/12028960/pexels-photo-12028960.jpeg" alt="">'
+    title.innerHTML = 'Lorem ipsum dolor sit amet.'
+    excerpt.innerHTML = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed provident pariatur animi distinctio incidunt vel!'
+    profile_img.innerHTML = '<img src="https://randomuser.me/api/portraits/men/45.jpg" alt="">'
+    name.innerHTML = 'John Doe'
+    date.innerHTML = 'June 22, 2022'
+}
+
