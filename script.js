bb0651b05d19c2afe33b47ef94599fac5db9e62e
raquellyc <raquels.castillo.78@gmail.com>
2022-06-22 21:56:45 -0600

    finish js

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ const date = document.getElementById('date')
 const animated_bgs = document.querySelectorAll('.animated-bg')
 const animated_bg_texts = document.querySelectorAll('.animated-bg-text')
 
+setTimeout(getData, 2500)
+
 function getData() {
     header.innerHTML = '<img src="https://images.pexels.com/photos/12028960/pexels-photo-12028960.jpeg" alt="">'
     title.innerHTML = 'Lorem ipsum dolor sit amet.'
@@ -15,5 +17,8 @@ function getData() {
     profile_img.innerHTML = '<img src="https://randomuser.me/api/portraits/men/45.jpg" alt="">'
     name.innerHTML = 'John Doe'
     date.innerHTML = 'June 22, 2022'
+
+    animated_bgs.forEach(bg => bg.classList.remove('animated-bg'))
+    animated_bg_texts.forEach(bg => bg.classList.remove('animated-bg-text'))
 }
 
